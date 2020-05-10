adf55098c09a7b441fcc505b83c8743f61e2cc1f
Kejuntrap <kfktit7@gmail.com>
2020-05-10 16:56:57 +0900

    implemented logical operation AND,OR

--- type.ml
+++ type.ml
@@ -1,5 +1,5 @@
 type exp =
-     Integer_ of int         (* リテラル *)
+  |  Integer_ of int         (* リテラル *)
   |  Plus of exp * exp     (* 足し算 *)
   |  Times of exp * exp    (* かけ算 *)
   |  Minus of exp * exp     (* ひき算 *)
@@ -7,6 +7,8 @@ type exp =
   |  Boolean_ of bool   (* boolean *)
   |  If of exp * exp * exp   (* if文 *)
   |  Eq of exp * exp        (* 等しいというやつ？e1 = e2 *)
+  |  Or of exp * exp      (* ここ論理演算 *)
+  |  And of exp * exp     (* ここ論理演算 *)
   |  Gre of exp * exp   (* 大なり A>Bに対してtrueを返す *)
 ;;
 
@@ -48,28 +50,43 @@ let rec eval1 e =
     begin match(eval1 e1 ,eval1 e2) with
     | (Integer(n1), Integer(n2)) -> Boolean(n1=n2)
     | (Boolean(b1), Boolean(b2)) -> Boolean(b1=b2)
-    | _ -> failwith "wrong value"
+    | _ -> failwith "wrong value at EQ"
   end
   | Gre(e1,e2) ->
     begin match (eval1 e1,eval1 e2) with
-    | (Integer(n1) , Integer(n1)) -> Boolean(n1>n2)
-    | _ -> failwith "worng value"
+    | (Integer(n1) , Integer(n2)) -> Boolean(n1>n2)
+    | _ -> failwith "worng value at GRE"
+  end
+  | Or(e1,e2) ->
+    begin match (eval1 e1,eval1 e2) with
+    | (Boolean(b1) , Boolean(b2)) -> 
+      if b1 = false && b2 = false then
+        Boolean(false)
+      else
+        Boolean(true)
+    | _ -> failwith "worng value at OR"
   end 
-  | If(e1,e2,e3) -> (* if(条件) 真のとき 偽の時 的な実装っぽい *)
+  | And(e1,e2) ->
+    begin match (eval1 e1,eval1 e2) with
+    | (Boolean(b1) , Boolean(b2)) ->
+      if b1 = true && b2 = true then
+        Boolean(true)
+      else
+        Boolean(false)
+    | _ -> failwith "worng value at AND"
+  end 
+  | If(e1,e2,e3) -> (* if(条件) 真のとき 偽の時 的な実装っぽい  excelのアレ *)
     begin match(eval1 e1) with
     | Boolean(true) -> eval1 e2
     | Boolean(false) -> eval1 e3
-    | _ -> failwith "wrong value"
+    | _ -> failwith "wrong value　at IF"
   end
 ;;
 
 
 let res = eval1(Plus( Integer_ 6 , Integer_ 7));;
-
 let res = eval1(Minus( Integer_ 6 , Integer_ 7));;
-
 let res = eval1(Times( Integer_ 6 , Integer_ 7));;
-
 let res = (Plus(Boolean_ true, Boolean_ true));;
 
 let _ = eval1 (Integer_ 1);;
@@ -83,4 +100,9 @@ let _ = eval1 (Eq (Integer_ 1, Integer_ 1));;
 let _ = eval1 (Eq (Integer_ 1, Integer_ 2));;
 let _ = eval1 (Eq (Boolean_ true, Boolean_ true));;
 let _ = eval1 (Eq (Boolean_ true, Boolean_ false));;
-let _ = eval1(Eq(Integer_ 1, Boolean_ true));;
+
+let _ = eval1(Gre(Integer_ 1, Integer_ 0));;
+let _ = eval1(And(Boolean_ false,Boolean_ false));;
+let _ = eval1(And(Boolean_ true,Boolean_ true));;
+let _ = eval1(Or(Boolean_ true,Boolean_ false));;
+let _ = eval1(Or(Boolean_ false,Boolean_ false));;
