e751ded3a204fffd83f62460792291bb46c64f38
Kejuntrap <kfktit7@gmail.com>
2020-05-10 15:17:55 +0900

    slightly changed

--- type.ml
+++ type.ml
@@ -7,6 +7,7 @@ type exp =
   |  Boolean_ of bool   (* boolean *)
   |  If of exp * exp * exp   (* if文 *)
   |  Eq of exp * exp        (* 等しいというやつ？e1 = e2 *)
+  |  Gre of exp * exp   (* 大なり A>Bに対してtrueを返す *)
 ;;
 
 (* 値の型　*)
@@ -23,17 +24,17 @@ let rec eval1 e =
     begin match(eval1 e1 , eval1 e2) with
     | (Integer(n1),Integer(n2)) -> Integer(n1+n2)
     | _ -> failwith "Integer values expected"
-    end
+  end
   | Times(e1,e2) -> 
     begin match(eval1 e1 , eval1 e2) with
     | (Integer(n1),Integer(n2)) -> Integer(n1*n2)
     | _ -> failwith "Integer values expected"
-    end
+  end
   | Minus(e1,e2) -> 
     begin match(eval1 e1 , eval1 e2) with
     | (Integer(n1),Integer(n2)) -> Integer(n1-n2)
     | _ -> failwith "Integer values expected"
-    end
+  end
   | Div(e1,e2) -> 
     begin match(eval1 e1 , eval1 e2) with
     | (Integer(n1),Integer(n2)) -> 
@@ -42,19 +43,24 @@ let rec eval1 e =
       else
         failwith "Division_by_zero"
     | _ -> failwith "Integer values expected"
-    end
+  end
   | Eq(e1,e2) ->
     begin match(eval1 e1 ,eval1 e2) with
     | (Integer(n1), Integer(n2)) -> Boolean(n1=n2)
     | (Boolean(b1), Boolean(b2)) -> Boolean(b1=b2)
     | _ -> failwith "wrong value"
-    end
+  end
+  | Gre(e1,e2) ->
+    begin match (eval1 e1,eval1 e2) with
+    | (Integer(n1) , Integer(n1)) -> Boolean(n1>n2)
+    | _ -> failwith "worng value"
+  end 
   | If(e1,e2,e3) -> (* if(条件) 真のとき 偽の時 的な実装っぽい *)
     begin match(eval1 e1) with
     | Boolean(true) -> eval1 e2
     | Boolean(false) -> eval1 e3
     | _ -> failwith "wrong value"
-    end
+  end
 ;;
 
 
@@ -64,9 +70,17 @@ let res = eval1(Minus( Integer_ 6 , Integer_ 7));;
 
 let res = eval1(Times( Integer_ 6 , Integer_ 7));;
 
-let res = eval1(Div( Integer_ 6 , Integer_ 0));;
-
-
 let res = (Plus(Boolean_ true, Boolean_ true));;
 
-let res = (If(Eq(Integer(7), Integer(6)), 0,1));;
+let _ = eval1 (Integer_ 1);;
+let _ = eval1 (Integer_ 11);;
+let _ = eval1 (Plus (Integer_ 1, Plus (Integer_ 2, Integer_ 11)));;
+let _ = eval1 (Times (Integer_ 1, Plus (Integer_ 2, Integer_ 11)));;
+let _ = eval1 (If (Eq(Integer_ 2, Integer_ 11),
+                   Times(Integer_ 1, Integer_ 2),
+                   Times(Integer_ 1, Plus(Integer_ 2,Integer_ 3))));;
+let _ = eval1 (Eq (Integer_ 1, Integer_ 1));;
+let _ = eval1 (Eq (Integer_ 1, Integer_ 2));;
+let _ = eval1 (Eq (Boolean_ true, Boolean_ true));;
+let _ = eval1 (Eq (Boolean_ true, Boolean_ false));;
+let _ = eval1(Eq(Integer_ 1, Boolean_ true));;
